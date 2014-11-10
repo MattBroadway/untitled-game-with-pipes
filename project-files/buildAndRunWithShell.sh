955d1fa485727bbe264c9b6890bcf4dd563bd545
Matthew Broadway <matthewbroadway@msn.com>
2014-11-10 22:53:29 +0000

    added option to update libararies during build (enabled by default in buildAndRunWithShell.sh)

--- project-files/buildAndRunWithShell.sh
+++ project-files/buildAndRunWithShell.sh
@@ -3,25 +3,35 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 outDir="out"
 
+
+while getopts ":lb" opt; do
+case "$opt" in
+	l)
+		echo Updating libraries
+		bash ./getLibs.sh
+		;;
+	b)
+		echo Building the game
+
+		cd $SCRIPTPATH/../
+		mkdir $outDir 2> /dev/null
+		cd src
+		javac *.java -classpath $SCRIPTPATH/../lib/out/ -d ../$outDir/ 2> ../$outDir/lastBuild.log
+
+		if [[ $? != 0 ]];then
+			less ../$outDir/lastBuild.log
+			echo There were errors in compilation. Aborting.
+
+			exit
+		fi
+		;;
+	\?)
+		echo "invalid option"
+		;;
+esac
+done
+
 echo Running game
-
-if [ "$1" = "-b" ]; then
-	echo Building game first
-
-	cd $SCRIPTPATH/../
-	mkdir $outDir 2> /dev/null
-	cd src
-	javac *.java -classpath $SCRIPTPATH/../lib/out/ -d ../$outDir/ 2> ../$outDir/lastBuild.log
-
-	if [[ $? != 0 ]];then
-		less ../$outDir/lastBuild.log
-		echo There were errors in compilation. Aborting.
-
-		exit
-	fi
-else
-	echo "(-b to build and run)"
-fi
 
 if [ ! -d "$SCRIPTPATH/../$outDir" ];then
 	echo You must build first
